f09acbabb66cc56f93d16199bd4f310d079fa916
santiago suescun <nspes2020@gmail.com>
2024-01-08 00:06:23 -0500

    updtae

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-
+const cors = require('cors');
 const app = express()
 
+app.use(cors());
 app.use(express.json());
 
 
@@ -13,4 +14,9 @@ app.use("/2",require("./movies1"));
 
 app.listen(3000)
 console.log(`Server on port ${3000}`);
+
+
+
+
+
 
